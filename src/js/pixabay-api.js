09592fd9392533d454e4933e1cd6c09a199b1f4f
OksanaGreen <SpringOcsana@ukr.net>
2024-08-09 21:28:03 +0300

    corekt

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,8 @@
 export function getPictures(query) {
   const BASE_URL = 'https://pixabay.com/api/';
   const params = new URLSearchParams({
-    key: '45354989-e1e4081af194e4d3149ed2a9d ',
+    // key: '45354989-e1e4081af194e4d3149ed2a9d ',
+    key: '42978821-e3c6f538b5791c0a766c3ba19',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
