68d3a6fa548baafff73ac315386231b399a8cadd
OksanaGreen <SpringOcsana@ukr.net>
2024-08-09 16:59:03 +0300

    debugging

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import { getPictures } from './js/pixabay-api.js';
 import { renderElement } from './js/render-functions.js';
 import { renderElements } from './js/render-functions.js';
 
-// Описаний у документації
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
 
 import SimpleLightbox from 'simplelightbox';
